0d1927b23692fc093c14adf98f838be62f81781a
Wesley <wesley.felix12@hotmail.com>
2026-06-30 20:44:06 -0300

    more practice 1.1.2

---
+++ exemplos/string/metodosString.py
@@ -0,0 +1,43 @@
+my_str = "hello world"
+
+capitalized_str = my_str.capitalize()
+print(capitalized_str)
+
+upper_str = my_str.upper()
+print(upper_str)
+
+lower_str = my_str.lower()
+print(lower_str)
+
+replaced_str = my_str.replace('hello', 'hi')
+print(replaced_str)
+
+split_str = my_str.split()
+print(split_str)
+
+joined_str = ' '.join(my_str)
+print(joined_str)
+
+striped_str = my_str.strip()
+print(striped_str)
+
+start_str = my_str.startswith('hello')
+print(start_str)
+
+end_str = my_str.endswith('universe')
+print(end_str)
+
+finding_str = my_str.find('world')
+print(finding_str)
+
+counted_str = my_str.count('o')
+print(counted_str)
+
+is_upper_str = my_str.isupper()
+print(is_upper_str)
+
+is_lower_str = my_str.islower()
+print(is_lower_str)
+
+titled_str = my_str.title()
+print(titled_str)
---
+++ exemplos/string/stringSlicing.py
@@ -0,0 +1,5 @@
+text = "Bom dia flor do dia"
+
+print(text[1:9])
+print(text[:6:2])
+print(text[0:11:3])
