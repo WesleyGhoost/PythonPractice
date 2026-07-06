3b9d6ab4714f2abc96a452f542ac5537ddc8aaa2
Wesley <wesley.felix12@hotmail.com>
2026-07-06 19:49:19 -0300

    more practice 1.2

---
+++ exemplos/numeros/atribuicao.py
@@ -0,0 +1,35 @@
+sum_number = 10
+sum_number += 5
+print(sum_number)
+
+sub_number = 20
+sub_number -= 10
+print(sub_number)
+
+multi_number = 30
+multi_number *= 5
+print(multi_number)
+
+div_number = 50
+div_number /= 10
+print(div_number)
+
+rest_number = 23
+rest_number %= 7
+print(rest_number)
+
+min_number = 12
+min_number //= 4
+print(min_number)
+
+pow_number = 5
+pow_number **= 6
+print(pow_number)
+
+sum_string= 'Hello '
+sum_string += ' World'
+print(sum_string)
+
+repeat_str = 'Hello'
+repeat_str *= 3
+print(repeat_str)
---
+++ exemplos/numeros/operacoes.py
@@ -0,0 +1,37 @@
+number_one = 14
+number_two = 33
+number_three = 10.2
+number_four = 27.6
+
+sumNumbers = number_one + number_two
+subNumbers = number_four - number_three
+multiNumbers = number_three * number_one
+divNumbers = number_two / number_three
+
+print(sumNumbers)
+print(subNumbers)
+print(multiNumbers)
+print(divNumbers)
+
+print(subNumbers // divNumbers)
+print(multiNumbers % divNumbers)
+print(sumNumbers ** subNumbers)
+
+str_one = '47'
+str_two = '7.09'
+
+print(int(str_one))
+print(float(str_two))
+
+
+method_number_one = 1.44
+method_number_two = -35
+
+roundedNumber = round(method_number_one)
+absNumber = abs(method_number_two)
+
+result_one = pow(2, 5)
+
+print(roundedNumber)
+print(absNumber)
+print(result_one)
